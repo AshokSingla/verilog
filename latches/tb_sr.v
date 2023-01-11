083d7fefa126620fafad2350a24599c82978be5d
Ashok Singla <79105219+AshokSingla@users.noreply.github.com>
2023-01-11 19:16:40 +0530

    Add files via upload

---
+++ latches/tb_sr.v
@@ -0,0 +1,42 @@
+module tb_sr (
+    
+);
+
+reg s,r,clock;
+wire q,qbar;
+
+sr_latch S(q,qbar,s,r,clock);
+
+initial begin
+    $monitor($time, " q = %b , qbar = %b , s = %b , r = %b",q,qbar,s,r);
+    clock = 1'b1;
+    #2;
+    s = 1'b0;
+    r = 1'b1;
+    #2;
+    s = 1'b0;
+    r = 1'b0;
+    #2;
+    s = 1'b1;
+    r = 1'b0;
+    #2;
+    s = 1'b0;
+    r = 1'b0;
+    #2;
+    s = 1'b1;
+    r = 1'b1;
+    #2;
+    s = 1'b0;
+    r = 1'b0;
+end
+
+initial begin
+    #12 $finish;
+end
+
+always begin
+    #1;
+    clock = ~clock;
+end
+    
+endmodule
