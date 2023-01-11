b844b38279f787595706126a776707a95f50afca
Ashok Singla <79105219+AshokSingla@users.noreply.github.com>
2023-01-12 00:02:45 +0530

    Add files via upload

---
+++ flip flops/d/tb_d.v
@@ -0,0 +1,24 @@
+module tb_dff();
+reg d, clock;
+wire q, qbar;
+
+d_master_slave D(q,qbar,d,clock);
+
+initial begin
+    $monitor($time, " q=%b, qbar=%b, d=%b",q,qbar,d);
+    clock = 1'b1;
+    d = 1'b0;
+    #2;
+    d = 1'b1;
+    #2;
+    d = 1'b1;
+    #2;
+    d = 1'b0;
+end
+
+always
+#1 clock = ~clock;
+
+initial
+#7 $finish;
+endmodule
