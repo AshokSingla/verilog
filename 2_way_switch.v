0991d20135b1a318aa7964e3f1f3bf107e845f7d
Ashok Singla <79105219+AshokSingla@users.noreply.github.com>
2023-01-29 16:20:45 +0530

    Add files via upload

---
+++ 2_way_switch.v
@@ -0,0 +1,25 @@
+module switch_2_way (
+    output reg out,
+    input in1,in2
+);
+
+always @(*) begin
+    case({in1,in2})
+    2'b00: out = 1'b0;
+    2'b01: out = 1'b1;
+    2'b10: out = 1'b1;
+    2'b11: out = 1'b0;
+    default: out = 1'bx;    
+    endcase
+end
+    
+endmodule
+
+module switch (
+    output out,
+    input in1,in2
+);
+
+assign out = in1^in2;
+
+endmodule
