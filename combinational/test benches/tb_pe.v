9b1ac335b1acfb831624e1b311b1eff7aefeed99
Ashok Singla <79105219+AshokSingla@users.noreply.github.com>
2023-02-14 19:53:16 +0530

    Update tb_pe.v

--- combinational/test benches/tb_pe.v
+++ combinational/test benches/tb_pe.v
@@ -1,18 +1,30 @@
 module tb_pe;
 
-reg [3:0]in;
-wire [1:0]out;
+reg [9:0]in;
+wire [3:0]out;
 wire valid;
 
-priority_encoder pe(out,valid,in);
+priority_encoder_dectobin pe(out,valid,in);
 
 initial begin
     $monitor("out = %b, in = %b",out,in);
-    in = 4'b1000;
+    in = 10'b1000000000;
     #1;
-    in[1] = 1'b1;
+    in[4] = 1'b1;
     #1;
-    in[3] = 1'b0;
+    in[7] = 1'b1;
+    #1;
+    in[9] = 1'b0;
+    #1;
+    in[5] = 1'b1;
+    #1;
+    in[7] = 1'b0;
+    #1;
+    in[5] = 1'b0;
+    #1;
+    in[4] = 1'b1;
+    #1;
+    in[4] = 1'b0;
 
 end
 
